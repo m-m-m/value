575a590177f4656754d17cca5fc6f73f8c64234a
Jörg Hohwiller <hohwille@users.sourceforge.net>
2022-01-16 23:58:36 +0100

    added CriteriaSelection

---
+++ core/src/main/java/io/github/mmm/value/CriteriaSelection.java
@@ -0,0 +1,12 @@
+package io.github.mmm.value;
+
+/**
+ * Interface for a selection (e.g. in a select statement). Can be a different things such as a {@link PropertyPath
+ * property}, a criteria expression or a literal value.
+ *
+ * @param <V> type of the value to select.
+ * @since 1.0.0
+ */
+public interface CriteriaSelection<V> {
+
+}
