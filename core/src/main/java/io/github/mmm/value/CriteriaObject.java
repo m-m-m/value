54972dfe5fd152add5ac4f3fb2028a574b2badde
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-01-04 00:16:09 +0100

    JavaDoc fix

--- core/src/main/java/io/github/mmm/value/CriteriaObject.java
+++ core/src/main/java/io/github/mmm/value/CriteriaObject.java
@@ -1,7 +1,7 @@
 package io.github.mmm.value;
 
 /**
- * Interface for a generic criteria expression. Can be a different things such as a {@link PropertyPath property}, a
+ * Interface for a generic criteria expression. Can be different things such as e.g. a {@link PropertyPath property}, a
  * criteria expression or a literal value.
  *
  * @param <V> type of the value to select.
